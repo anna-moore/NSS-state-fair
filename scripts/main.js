2f86f3f0fb157a64c0712ca7cfc99804c7c37c7d
anna-moore <ammoore434@gmail.com>
2020-12-08 15:56:42 -0600

    ticket package

--- scripts/main.js
+++ scripts/main.js
@@ -4,9 +4,11 @@ import { RideTicketHolders } from "./rides/RideTicketHolders.js"
 import { TicketBooth } from "./TicketBooth.js"
 import { FoodTicketHolders } from "./food/FoodTicketHolder.js"
 import { SideshowTicketHolders } from "./sideshows/SideshowTicketHolder.js"
+import { FullPackageTicketHolders } from "./FullPackageTicket.js"
 
 TicketBooth()
 RideTicketHolders()
 GameTicketHolders()
 FoodTicketHolders()
 SideshowTicketHolders()
+FullPackageTicketHolders()
